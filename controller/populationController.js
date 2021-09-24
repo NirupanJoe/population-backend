1317482ed1386a759c0c26d5f7067591d7db1f9a
NirupanJoe <nirupanjoej@gmail.com>
2021-09-24 05:59:43 +0000

    Introduced deletePopulation.

--- controller/populationController.js
+++ controller/populationController.js
@@ -1,4 +1,5 @@
 const population = require('../models').Population;
+const { Op } = require('sequelize');
 
 exports.createPopulation = async(req, res) => {
 	const result = await population.create({...req.body});
@@ -8,6 +9,18 @@ exports.createPopulation = async(req, res) => {
 
 exports.getPopulation = async(req, res) => {
 	const result = await population.findAll();
+
+	res.json(result);
+};
+
+exports.deletePopulation = async(req, res) => {
+	const result = await population.destroy({
+		where: {
+			id: {
+				[Op.eq]: req.params.id,
+			},
+		},
+	});
 
 	res.json(result);
 };
--- router/populationRouter.js
+++ router/populationRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { createPopulation, getPopulation } = require('../controller/populationController');
+const { createPopulation, getPopulation, deletePopulation } = require('../controller/populationController');
 
 const router = Router();
 
 router.route('/').post(createPopulation).get(getPopulation);
+router.route('/:id').delete(deletePopulation);
 
 module.exports = router;
